bf68db93730ad9afccc5c992c33fca4d4fedd65e
Stein Heselmans <teh@melexis.com>
2017-09-21 21:19:15 +0200

    Add some integration test case

---
+++ tests/test_integration.py
@@ -0,0 +1,21 @@
+from unittest import TestCase
+
+from duco.ducobox import ducobox_wrapper
+
+
+class TestIntegration(TestCase):
+
+    def test_help(self):
+        with self.assertRaises(SystemExit) as ex:
+            ducobox_wrapper(['--help'])
+        self.assertEqual(0, ex.exception.code)
+
+    def test_version(self):
+        with self.assertRaises(SystemExit) as ex:
+            ducobox_wrapper(['--version'])
+        self.assertEqual(0, ex.exception.code)
+
+    def test_no_port(self):
+        with self.assertRaises(SystemExit) as ex:
+            ducobox_wrapper([])
+        self.assertEqual(2, ex.exception.code)
