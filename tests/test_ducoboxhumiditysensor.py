7ff04634f11cb67e109712e107771c3158fe8c0c
Stein Heselmans <teh@melexis.com>
2017-10-10 20:34:10 +0200

    Bind interface to node

--- tests/test_ducoboxhumiditysensor.py
+++ tests/test_ducoboxhumiditysensor.py
@@ -13,11 +13,12 @@ class TestDucoBoxHumiditySensor(TestCase):
     def test_happy(self, itf_mock):
         sensor = dut.DucoBoxHumiditySensor(1, 2)
         itf_mock_object = MagicMock(spec=dut.DucoInterface)
+        sensor.bind(itf_mock_object)
 
         with open('tests/cmd_sensorinfo.txt') as cmdfile:
-            itf_mock_object._execute.return_value = cmdfile.read().replace('\n', '\r')
-        sensor.sample(itf_mock_object)
-        itf_mock_object._execute.assert_called_once_with('sensorinfo\r')
+            itf_mock_object.execute_command.return_value = cmdfile.read().replace('\n', '\r')
+        sensor.sample()
+        itf_mock_object.execute_command.assert_called_once_with('sensorinfo\r')
 
         self.assertEqual(sensor.humidity, 68.37)
         self.assertEqual(sensor.temperature, 18.9)
